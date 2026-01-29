758a72bac563e6f36a84d2a37a3130406aa5a373
Christophe <christophe.tardif81@gmail.com>
2026-01-29 09:52:22 +0100

    ajout: sortie de l'app à la fermeture de la fenêtre

--- src/tuto_git/Contrôleur.java
+++ src/tuto_git/Contrôleur.java
@@ -2,8 +2,10 @@ package tuto_git;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class Contrôleur implements ActionListener {
+public class Contrôleur implements ActionListener, WindowListener {
 
 	Modèle refModl;
 	
@@ -15,6 +17,33 @@ public class Contrôleur implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		refModl.incrémente();
 	}
-
-	
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub		
+	}
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		System.exit(0);
+	}
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub	
+	}
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+	}
 }
